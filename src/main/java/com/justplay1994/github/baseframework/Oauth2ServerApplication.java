ce4f0f39422781400aa09d325def5928d5212733
huangzezhou <764804602@qq.com>
2018-10-24 22:30:44 +0800

    配置文件修改

--- src/main/java/com/justplay1994/github/baseframework/Oauth2ServerApplication.java
+++ src/main/java/com/justplay1994/github/baseframework/Oauth2ServerApplication.java
@@ -3,12 +3,16 @@ package com.justplay1994.github.baseframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @ComponentScan
+@EnableAuthorizationServer
+@EnableResourceServer
 public class Oauth2ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BaseFrameworkApplication.class, args);
+		SpringApplication.run(Oauth2ServerApplication.class, args);
 	}
 }
